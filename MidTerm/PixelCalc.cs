370a14ec06a1bacb905edacf699fc160920d17e5
PrashantV2 <prashantvirdi719@gmail.com>
2023-06-23 12:19:26 -0400

    Add project files.

---
+++ MidTerm/PixelCalc.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MidTerm
+{
+    public static class PixelCalc
+    {
+        public static Dictionary<string, double> PixelCalcFunction(double height, double width, double diagnal)
+        {
+            double diagnalResult = Math.Sqrt(height * height + width * width);
+
+            double dotPath = diagnal / diagnalResult;
+
+            double ppi = diagnalResult / diagnal;
+            var result = new Dictionary<string, double>();
+            result["PPI"] = ppi;
+            result["Diagonal"] = diagnalResult;
+            result["Dot"] = dotPath;
+            return result;
+
+
+        }
+    }
+}
